f8a4b549a23557aa0441f5d83e5f249af8b097b5
htoh702 <htoh702@gmail.com>
2022-09-05 17:19:52 +0900

    six

--- instagram_clone/views.py
+++ instagram_clone/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .models import Feed
+from .models import Feed, Reply, Like, Bookmark
 from rest_framework.response import Response
 import os
 from djangoProject.settings import MEDIA_ROOT
@@ -11,21 +11,40 @@ from django.contrib.auth.hashers import check_password
 
 class Main(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all().order_by('-id')  # select * from instagram_clone_feed;//query
-        # .order_by('-id') id를 역순으로 가져온다
-
         email = request.session.get('email', None)
 
         if email is None:
             return render(request, 'user/login.html')
 
+        feed_object_list = Feed.objects.all().order_by('-id')  # select  * from content_feed;
+        feed_list = []
+
+        for feed in feed_object_list:
+            user1 = User.objects.filter(email=feed.email).first()
+            reply_object_list = Reply.objects.filter(feed_id=feed.id)
+            reply_list = []
+            for reply in reply_object_list:
+                user = User.objects.filter(email=reply.email).first()
+                reply_list.append(dict(reply_content=reply.reply_content,
+                                       user_id=user.user_id))
+            like_count = Like.objects.filter(feed_id=feed.id, is_like=True).count()
+            is_liked = Like.objects.filter(feed_id=feed.id, email=email, is_like=True).exists()
+            is_marked = Bookmark.objects.filter(feed_id=feed.id, email=email, is_marked=True).exists()
+            feed_list.append(dict(id=feed.id,
+                                  image=feed.image,
+                                  content=feed.content,
+                                  profile_image=user1.profile_image,
+                                  user_id=user1.user_id,
+                                  like_count=like_count,
+                                  reply_list=reply_list,
+                                  is_liked=is_liked,
+                                  is_marked=is_marked
+                                  ))
+
         user = User.objects.filter(email=email).first()
 
         if user is None:
             return render(request, 'user/login.html')
-
-        for feed in feed_list:
-            print(feed.content)
 
         return render(request, 'instagram_clone/Main.html', context=dict(feed=feed_list, user=user))  # 3번째 인자는 데이터가 사용될 때 작성한다.(댁셔너리로 작성)
 
@@ -55,28 +74,25 @@ class Main(APIView):
 class UploadFeed(APIView):
     def post(self, request):
         file = request.FILES['file']
-        uuid_name = uuid4().hex     # random 으로 이름을 생성
+
+        uuid_name = uuid4().hex
         save_path = os.path.join(MEDIA_ROOT, uuid_name)
 
         with open(save_path, 'wb+') as destination:
             for chunk in file.chunks():
                 destination.write(chunk)
 
-        content = request.data.get('content')
         image = uuid_name
-        profile_image = request.data.get('profile_image')
-        user_id = request.data.get('user_id')
+        content = request.data.get('content')
+        email = request.session.get('email', None)
 
-        Feed.objects.create(content=content, image=image, profile_image=profile_image, user_id=user_id, like_count=0)
+        Feed.objects.create(content=content, image=image, email=email)
 
-        return Response(status=200)     # 성공을 의미하는 200
+        return Response(status=200)
 
 
 class Profile(APIView):
     def get(self, request):
-        feed_list = Feed.objects.all().order_by('-id')  # select * from instagram_clone_feed;//query
-        # .order_by('-id') id를 역순으로 가져온다
-
         email = request.session.get('email', None)
 
         if email is None:
@@ -87,10 +103,68 @@ class Profile(APIView):
         if user is None:
             return render(request, 'user/login.html')
 
-        for feed in feed_list:
-            print(feed.content)
+        feed_list = Feed.objects.filter(email=email).all()
+        like_list = list(Like.objects.filter(email=email, is_like=True).values_list('feed_id', flat=True))
+        like_feed_list = Feed.objects.filter(id__in=like_list)
+        bookmark_list = list(Bookmark.objects.filter(email=email, is_marked=True).values_list('feed_id', flat=True))
+        bookmark_feed_list = Feed.objects.filter(id__in=bookmark_list)
 
-        return render(request, 'instagram_clone/profile.html', context=dict(feed=feed_list, user=user))  # 3번째 인자는 데이터가 사용될 때 작성한다.(댁셔너리로 작성)
+        return render(request, 'instagram_clone/profile.html', context=dict(feed=feed_list, user=user, ))  # 3번째 인자는 데이터가 사용될 때 작성한다.(댁셔너리로 작성)
 
+
+class UploadReply(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        reply_content = request.data.get('reply_content', None)
+        email = request.session.get('email', None)
+
+        Reply.objects.create(feed_id=feed_id, reply_content=reply_content, email=email)
+
+        return Response(status=200)
+
+
+class ToggleLike(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        favorite_text = request.data.get('favorite_text', True)
+
+        if favorite_text == 'favorite':
+            is_like = False
+        else:
+            is_like = True
+
+        email = request.session.get('email', None)
+
+        like = Like.objects.filter(feed_id=feed_id, email=email).first()
+
+        if like:
+            like.is_like = is_like
+            like.save()
+        else:
+            Like.objects.create(feed_id=feed_id, is_like=is_like, email=email)
+
+        return Response(status=200)
+
+
+class CheckBookmark(APIView):
+    def post(self, request):
+        feed_id = request.data.get('feed_id', None)
+        bookmark_text = request.data.get('bookmark_text', True)
+        email = request.session.get('email', None)
+
+        if bookmark_text == 'bookmark':
+            is_marked = False
+        else:
+            is_marked = True
+
+        mark = Bookmark.objects.filter(feed_id=feed_id, email=email).first()
+
+        if mark:
+            mark.is_marked = is_marked
+            mark.save()
+        else:
+            Bookmark.objects.create(feed_id=feed_id, is_marked=is_marked, email=email)
+
+        return Response(status=200)
 
 # Create your views here.
