12809bcca25f3765d9c6f013e903deeb87b59ca7
htoh702 <htoh702@gmail.com>
2023-07-17 16:05:22 +0900

    update

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+from pip._internal.network import auth
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,10 +79,25 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'   : 'django.db.backends.mysql',
+        'NAME'     : 'db_instagram_clone',
+        'USER'     : 'root',
+        'PASSWORD' : '2910oh5668',
+        'HOST'     : 'localhost',
+        'OPTIONS'  : {'charset': 'utf8mb4'},
+	    'TEST': {
+	    'CHARSET'  : 'utf8mb4',
+	    'COLLATION': 'utf8_general_ci'
+	    }
     }
 }
 
